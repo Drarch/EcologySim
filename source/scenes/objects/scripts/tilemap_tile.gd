1dcee2c68b0bdf62ebf6152f604b1b8888d13b48
Tomasz Szymański <cl.arctos.orion@gmail.com>
2020-08-28 19:57:34 +0200

    ADD: Tile plant eat, grow, view

--- source/scenes/objects/scripts/tilemap_tile.gd
+++ source/scenes/objects/scripts/tilemap_tile.gd
@@ -1,16 +1,41 @@
 extends Reference
 class_name Tile
 
+enum TileView { GRASS = 0, NO_GRASS = 1}
+
+
 var _map: TileMap
 
 var position: Vector2
 var animal: Node2D
+var maxPlants: int = 1
 var plants: int = 1
 
-func _init(map: TileMap, _position: Vector2, var _plants: int = 1):
+func _init(map: TileMap, _position: Vector2, var _maxPlants: int = 1):
 	_map = map
 	position = _position
-	plants = _plants
+	maxPlants = _maxPlants
 
-func regrow():
-	pass
+	_updateView()
+
+
+func regrow(amount: int):
+	plants = plants + amount
+	plants = int( min(plants, maxPlants) )
+	_updateView()
+
+func eat(amount: int = 1) -> bool:
+	if plants >= amount:
+		plants -= amount
+
+		_updateView()
+		return true
+
+	return false
+
+
+func _updateView():
+	if plants > 0:
+		_map.set_cellv(position, TileView.GRASS)
+	else:
+		_map.set_cellv(position, TileView.NO_GRASS)
